3f1e79636092b2e2ba02ca946aa1c8c5d2055527
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-06-05 00:38:10 +0900

    $this

--- src/Utility.php
+++ src/Utility.php
@@ -12,16 +12,16 @@ class Utility {
       $radius = 0;
       $center_position = $positions[0];
     } else if (count($positions) === 2) {
-      $radius = dist($positions) / 2.0;
+      $radius = $this->dist($positions) / 2.0;
       $center_positions = [(float)($positions[0][0] + $positions[1][0]) / 2.0, (float)($positions[0][1] + $positions[1][1]) / 2.0];
     } else {
       for ($first_index = 0; $first_index < count($positions); $first_index++) {
         for ($second_index = $first_index + 1; $second_index < count($positions); $second_index++) {
           for ($third_index = $second_index + 1; $third_index < count($positions); $third_index++) {
-            $circle = getCircleContainingThreePositions([$positions[$first_index], $positions[$second_index], $positions[$third_index]]);
+            $circle = $this->getCircleContainingThreePositions([$positions[$first_index], $positions[$second_index], $positions[$third_index]]);
             $is_containing_all_positions = true;
             for ($index = 0; $index < count($positions); $index++) {
-              if (!isWithinCircle($circle['center_position'], $circle['radius'], $positions[$index])) {
+              if (!$this->isWithinCircle($circle['center_position'], $circle['radius'], $positions[$index])) {
                 $is_containing_all_positions = false;
                 break;
               }
@@ -41,7 +41,7 @@ class Utility {
 
   // 返値：点が指定した円に含まれるかどうか（boolean）
   public function isWithinCircle($center_position, $radius, $target_position) {
-    if (dist([$target_position, $center_position]) <= $radius) {
+    if ($this->dist([$target_position, $center_position]) <= $radius) {
       return true;
     }
     return false;
@@ -58,7 +58,7 @@ class Utility {
       / 2.0;
     $center_position[0] = (float)($positions[0][0] + $positions[1][0]) / 2.0 + $t * (float)($positions[1][1] - $positions[0][1]);
     $center_position[1] = (float)($positions[0][1] + $positions[1][1]) / 2.0 - $t * (float)($positions[1][0] - $positions[0][0]);
-    $radius = dist([$positions[0], $center_position]);
+    $radius = $this->dist([$positions[0], $center_position]);
 
     return ['center_position' => $center_position, 'radius' => $radius];
   }
@@ -69,7 +69,7 @@ class Utility {
     $center_positions = [];
     $radius = 0;
 
-    $lines = [dist([$positions[0], $positions[1]]), dist([$positions[1], $positions[2]]), dist([$positions[2], $positions[0]])];
+    $lines = [$this->dist([$positions[0], $positions[1]]), $this->dist([$positions[1], $positions[2]]), $this->dist([$positions[2], $positions[0]])];
 
     $max_line_index = 0;
     for ($index = 1; $index < count($lines); $index++) {
@@ -88,7 +88,7 @@ class Utility {
 
     // 鋭角三角形または直角三角形の場合
     if (pow($lines[$max_line_index], 2) <= pow($lines[$other_line_indexes[0]], 2) + pow($lines[$other_line_indexes[1]], 2)) {
-      $circle = getCirclePassingThreePositions($positions);
+      $circle = $this->getCirclePassingThreePositions($positions);
       $radius = $circle['radius'];
       $center_position = $circle['center_position'];
     } else { // 鈍角三角形の場合 
