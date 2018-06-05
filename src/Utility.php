e3a5204a3db550a92e3b433ce5e917168e7110e2
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-06-05 12:53:55 +0900

    getFixations

--- src/Utility.php
+++ src/Utility.php
@@ -106,4 +106,90 @@ class Utility {
     $y_dist = abs($positions[1][1] - $positions[0][1]);
     return sqrt(pow($x_dist, 2) + pow($y_dist, 2));
   }
+
+  // 引数：$dpiは1インチ当たりのドット数，$precisionは計測誤差（度），
+  // $distanceは目からディスプレイまでの距離（cm），$flickは固視微動（度），
+  // $min_durationは注視したとする最低時間（ミリ秒），$filenameは読み込むCSVファイル（1列目X座標2列目Y座標3列目タイムスタンプ（ミリ秒））
+  public function getFixationCount($dpi, $precision, $distance, $flick, $min_duration, $filename) {
+    $fixations = $this->getFixations($dpi, $precision, $distance, $flick, $min_duration, $filename);
+    return count($fixations);
+  }
+
+  // 引数：$dpiは1インチ当たりのドット数，$precisionは計測誤差（度），
+  // $distanceは目からディスプレイまでの距離（cm），$flickは固視微動（度），
+  // $min_durationは注視したとする最低時間（ミリ秒），$filenameは読み込むCSVファイル（1列目X座標2列目Y座標3列目タイムスタンプ（ミリ秒））
+  public function getInitialFixation($dpi, $precision, $distance, $flick, $min_duration, $filename) {
+    $fixations = $this->getFixations($dpi, $precision, $distance, $flick, $min_duration, $filename);
+    if (count($fixations) === 0) {
+      return null;
+    }
+    return $fixations[0];
+  }
+
+  // 引数：$dpiは1インチ当たりのドット数，$precisionは計測誤差（度），
+  // $distanceは目からディスプレイまでの距離（cm），$flickは固視微動（度），
+  // $min_durationは注視したとする最低時間（ミリ秒），$filenameは読み込むCSVファイル（1列目X座標2列目Y座標3列目タイムスタンプ（ミリ秒））
+  public function getFixations($dpi, $precision, $distance, $flick, $min_duration, $filename) {
+    $dpc = $dpi / 2.54; // 1センチ当たりのドット数
+    $precision_error_range_cm = $distance * tan(deg2rad($precision)); // 計測誤差範囲（cm）
+    $flick_range_cm = $distance * tan(deg2rad($flick)); // 固視微動範囲（cm）
+    $range_cm = $precision_error_range_cm + $flick_range_cm; // 計測誤差範囲+固視微動範囲（cm）
+    $range_px = $range_cm * $dpc;
+  
+    $file = fopen($filename, 'r');
+    $recordings = [];
+    while ($line = fgetcsv($file)) {
+      // 同時刻のものは1つだけ格納する
+      if (count($recordings) > 0) {
+        if ($recordings[count($recordings) - 1][2] === $line[2]) {
+          continue;
+        }
+      }
+      $recordings[] = $line;
+    }
+  
+    $fixations = [];
+  
+    for ($first_index = 0; $first_index < count($recordings); $first_index++) {
+      $recording_stack = [];
+      for ($second_index = $first_index; $second_index < count($recordings); $second_index++) {
+        $recording_stack[] = $recordings[$second_index];
+        $circle = $this->getCircleContainingAllPositions($recording_stack);
+    
+        // 範囲外になった場合
+        if ($circle['radius'] > $range_px) {
+          array_pop($recording_stack);
+          $duration = $recording_stack[count($recording_stack) - 1][2] - $recording_stack[0][2];
+          if ($duration > MIN_DURATION) {
+            $circle = $this->getCircleContainingAllPositions($recording_stack);
+            $fixations[] = [
+              'center_position' => $circle['center_position'],
+              'start_time' => $recording_stack[0][2],
+              'end_time' => $recording_stack[count($recording_stack) - 1][2],
+              'duration' => $duration
+            ];
+            $first_index = $second_index - 1;
+          }
+          break;
+        }
+    
+        // 最終行の記録の場合
+        if ($second_index + 1 >= count($recordings)) {
+          $duration = $recording_stack[count($recording_stack) - 1][2] - $recording_stack[0][2];
+          if ($duration > MIN_DURATION) {
+            $fixations[] = [
+              'center_position' => $circle['center_position'],
+              'start_time' => $recording_stack[0][2],
+              'end_time' => $recording_stack[count($recording_stack) - 1][2],
+              'duration' => $duration
+            ];
+            $first_index = $second_index;
+          }
+          break;
+        }
+      }
+    }
+  
+    return $fixations;
+  }
 }
